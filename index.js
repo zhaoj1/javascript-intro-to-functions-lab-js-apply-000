97b2b420152441a715e183ad70518338cfc087ea
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-19 18:01:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 
+function sayHiToGrandma(string){
+  if(string==string.toLowerCase()){
+    return "I can't hear you!"
+  }
+}
